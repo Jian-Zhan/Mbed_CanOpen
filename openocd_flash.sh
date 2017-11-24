e38ee561409cf636a46d5fd7011fe3a277689807
Jian Zhan <zhanjian@gmail.com>
2017-11-24 22:22:32 +1000

    Working blinky / CAN sender for TAOBAO_STM32F103RB

---
+++ openocd_flash.sh
@@ -0,0 +1,14 @@
+openocd \
+    -f /opt/local/share/openocd/scripts/interface/stlink-v2.cfg \
+    -f /opt/local/share/openocd/scripts/target/stm32f1x.cfg \
+    -c init \
+    -c adapter_khz 100 \
+    -c targets \
+    -c "reset run" \
+    -c "sleep 100" \
+    -c "halt" \
+    -c "sleep 100" \
+    -c "flash write_image erase BUILD/TAOBAO_F103RB/GCC_ARM/Mbed_CanOpen.elf" \
+    -c "verify_image BUILD/TAOBAO_F103RB/GCC_ARM/Mbed_CanOpen.elf" \
+    -c "reset run" -c shutdown
+
